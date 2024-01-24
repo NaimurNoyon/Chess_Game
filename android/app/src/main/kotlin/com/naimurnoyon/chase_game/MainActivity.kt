797e9463cdd74a5e1ac4df4035d9edae103bda12
Naimur Noyon <57313497+NaimurNoyon@users.noreply.github.com>
2024-01-24 18:39:49 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/naimurnoyon/chase_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naimurnoyon.chase_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/game_board_screen.dart
@@ -0,0 +1,27 @@
+import 'package:chase_game/components/square.dart';
+import 'package:flutter/material.dart';
+
+import '../helper/helper_method.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+          itemCount: 8 * 8,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8),
+          itemBuilder: (context, index) {
+            return Square(isWhite: isWhite(index));
+          }),
+    );
+  }
+}
