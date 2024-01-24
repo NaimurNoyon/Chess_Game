544298023d77fb115394fa38522ece9cb558aad2
NaimurNoyon <naimurrahmannoyon@gmail.com>
2024-01-25 00:27:54 +0600

    xxx

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -1,13 +1,21 @@
+import 'package:chase_game/components/piece.dart';
 import 'package:flutter/material.dart';
 
 class Square extends StatelessWidget {
   final bool isWhite;
-  const Square({super.key, required this.isWhite});
+  final ChessPiece? piece;
+  const Square({super.key, required this.isWhite, required this.piece});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: isWhite? Colors.grey[200] : Colors.grey[500],
+      padding: const EdgeInsets.only(top: 5.0),
+      color: isWhite? Colors.grey[400] : Colors.grey[600],
+      child: piece!= null ?
+      Image.asset(
+        piece!.imagePath,
+        color: piece!.isWhite ? Colors.white : Colors.black,
+      ) : null,
     );
   }
 }
--- lib/screens/game_board_screen.dart
+++ lib/screens/game_board_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chase_game/components/piece.dart';
 import 'package:chase_game/components/square.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,115 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
+
+  late List<List<ChessPiece?>> board;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initializedBoard();
+  }
+
+  void _initializedBoard(){
+    List<List<ChessPiece?>> newBoard = List.generate(8, (index) => List.generate(8, (index) => null));
+
+    //pawn
+    for(int i = 0; i < 8; i++){
+      newBoard[1][i] = ChessPiece(
+          type: ChessPieceType.pawn,
+          isWhite: false,
+          imagePath: "assets/images/pawn.png",
+      );
+      newBoard[6][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: true,
+        imagePath: "assets/images/pawn.png",
+      );
+    }
+
+    //rook
+    newBoard[0][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: "assets/images/rook.png",
+    );
+
+    newBoard[0][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: "assets/images/rook.png",
+    );
+
+    newBoard[7][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: "assets/images/rook.png",
+    );
+
+    newBoard[7][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: "assets/images/rook.png",
+    );
+
+    //knight
+    newBoard[0][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: "assets/images/knight.png",
+    );
+
+    newBoard[0][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: "assets/images/knight.png",
+    );
+
+    newBoard[7][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: "assets/images/knight.png",
+    );
+
+    newBoard[7][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: "assets/images/knight.png",
+    );
+
+
+    //bishops
+    newBoard[0][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: "assets/images/bishop.png",
+    );
+
+    newBoard[0][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: "assets/images/bishop.png",
+    );
+
+    newBoard[7][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: "assets/images/bishop.png",
+    );
+
+    newBoard[7][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: "assets/images/bishop.png",
+    );
+
+
+
+
+    board = newBoard;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +130,14 @@ class _GameBoardState extends State<GameBoard> {
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 8),
           itemBuilder: (context, index) {
-            return Square(isWhite: isWhite(index));
+
+            int row = index ~/ 8;
+            int column = index % 8;
+
+            return Square(
+                isWhite: isWhite(index),
+              piece: board[row][column],
+            );
           }),
     );
   }
